0db04608c748b92a104d0a5441f0dfa1f49d7e12
Ivan Komissarov <ABBAPOH@gmail.com>
2017-06-26 19:02:34 +0300

    Add simple mainwindow

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,8 +1,13 @@
 #include "application.h"
 
+#include "mainwindow.h"
+
 int main(int argc, char *argv[])
 {
     Application app(argc, argv);
+
+    MainWindow w;
+    w.show();
 
     return app.exec();
 }
---
+++ src/app/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QtCore/QCoreApplication>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->actionQuit, &QAction::triggered, qApp, &QCoreApplication::quit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
